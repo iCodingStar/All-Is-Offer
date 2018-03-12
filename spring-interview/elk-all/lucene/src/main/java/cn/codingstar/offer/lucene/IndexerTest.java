3a1c1f681b2421440a45cb5dc7ca50ec90f041b6
iCodingStar <shixing.cs@gmail.com>
2018-03-12 17:58:52 +0800

    update

---
+++ spring-interview/elk-all/lucene/src/main/java/cn/codingstar/offer/lucene/IndexerTest.java
@@ -0,0 +1,19 @@
+package cn.codingstar.offer.lucene;
+
+/**
+ * @author: CodingStar
+ * @contact: shixing.cs@gmail.com
+ * @site: http://www.codingstar.cn
+ * @file: IndexerTest.java
+ * @time: 18-3-12 下午5:58
+ * @software: IntelliJ IDEA
+ * @version: 1.0
+ * @resource:
+ * @desc:
+ */
+public class IndexerTest {
+
+    public static void main(String[] args) {
+        
+    }
+}
