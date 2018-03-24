4f2ef2476cb76cd4a267030754d2407c7df8a5d4
iCodingStar <shixing.cs@gmail.com>
2018-03-24 11:36:27 +0800

    排序算法复习:插入排序优化

--- spring-interview/algorithm/src/main/java/cn/codingstar/offer/_sort/InsertSort.java
+++ spring-interview/algorithm/src/main/java/cn/codingstar/offer/_sort/InsertSort.java
@@ -33,16 +33,34 @@ public class InsertSort extends BaseSort {
         }
     }
 
+    /**
+     * 1, 8, 3, 5, 9, 4, 2, 7, 6,
+     *
+     * @param array
+     */
     @Override
     public void _sort(int[] array) {
-
+        if (array == null || array.length < 2) {
+            return;
+        }
+        for (int i = 1; i < array.length; i++) {
+            int j = i;
+            // 保存即将插入的元素
+            int temp = array[i];
+            // 条件必须写在这里
+            for (; j > 0 && temp < array[j - 1]; j--) {
+                // 将大元素后移,给目标元素找位置
+                array[j] = array[j - 1];
+            }
+            array[j] = temp;
+        }
     }
 
 
     public static void main(String[] args) {
         Sort sort = new InsertSort();
         int[] array = BaseSort.dataSet();
-        sort.sort(array);
+        sort._sort(array);
         sort.print(array);
     }
 
