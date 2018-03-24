6918c98a3fe5c57dbbc1398e952199e8cc59ea44
iCodingStar <shixing.cs@gmail.com>
2018-03-24 10:53:56 +0800

    排序算法复习

---
+++ spring-interview/algorithm/src/main/java/cn/codingstar/offer/_sort/Sort.java
@@ -0,0 +1,22 @@
+package cn.codingstar.offer._sort;
+
+/**
+ * @version: java8
+ * @author: CodingStar
+ * @contact: shixing.cs@gmail.com
+ * @file: Sort.java
+ * @time: 2018/2/28 15:03
+ * @software: Intellij Idea
+ * @desc:
+ */
+public interface Sort {
+
+    void sort(int[] array);
+
+    void _sort(int[] array);
+
+    void print(int[] array);
+
+    void swap(int[] array, int _index, int index_);
+
+}
