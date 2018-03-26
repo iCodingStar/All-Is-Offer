16f9da27671357bd82e833c7ec95d8bf945faa94
iCodingStar <shixing.cs@gmail.com>
2018-03-26 21:17:48 +0800

    String：判断两个字符串是否是相同字符串的重组(优化)

--- spring-interview/algorithm/src/main/java/cn/codingstar/handbook/String/Anagrams.java
+++ spring-interview/algorithm/src/main/java/cn/codingstar/handbook/String/Anagrams.java
@@ -44,8 +44,6 @@ public class Anagrams {
         int[] map = new int[256];
         for (int i = 0; i < s.length(); i++) {
             map[s.charAt(i)]++;
-        }
-        for (int i = 0; i < t.length(); i++) {
             map[t.charAt(i)]--;
         }
         for (int i = 0; i < 256; i++) {
